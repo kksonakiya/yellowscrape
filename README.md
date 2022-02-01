d4c66b19729fa5898d1e56ac2e527c57601e9a44
Krishnakant Sonakiya <30476198+kksonakiya@users.noreply.github.com>
2022-02-01 10:57:24 +0530

    Scraped the first page successfully! Base script is ready

---
+++ README.md
@@ -0,0 +1,14 @@
+Yellow Pages scraping script
+
+This script is going to scrape Yellow Pages.
+
+Sample URL:
+https://www.yellowpages.com/search?search_terms=james&geo_location_terms=TX&page=5&format=json
+
+Approach:
+
+1. Yellow Pages is going to open.
+2. We will change the name & cycle through the geo location shortcode.
+3. We will then cycle the name & again cycle the geo location.
+4. We'll save the info in csv.
+
---
+++ run.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+path='webdriver/chromedriver.exe'
+driver=webdriver.Chrome(path)
+
+driver.get('https://www.yellowpages.com/search?search_terms=james&geo_location_terms=TX')
+
+
+# We'll get the details of the first page.
+
+results=driver.find_elements_by_class_name('result')
+print('Results: ', len(results))
+for result in results:
+    businessName=result.find_element_by_class_name('business-name')
+    bname=businessName.text
+    print(bname)
